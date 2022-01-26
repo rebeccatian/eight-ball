6583bbaff2e2406f0bb751acb9b9de7e2b26527a
rebeccatian <97220971+rebeccatian@users.noreply.github.com>
2022-01-26 00:41:40 -0500

    Update eightBall.js

--- eightBall.js
+++ eightBall.js
@@ -32,12 +32,16 @@ switch (randomNumber) {
   break
   case 4 :
   eightBall = 'Do not count on it.'
+  break
   case 5 : 
   eightBall = 'My sources say no.'
+  break
   case 6 :
   eightBall = 'Outlook not so good.'
+  break
   case 7 : 
   eightBall = 'Signs point to yes.'
+  break
 }
 return eightBall;
 }
